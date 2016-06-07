e7f1671518dbecf3358cadccbc1c80924148eb64
Lane Winham <mlwinham12@gmail.com>
2016-06-07 16:10:48 -0600

    added find by name for enrollment repo

--- test/enrollment_repository_test.rb
+++ test/enrollment_repository_test.rb
@@ -1,13 +1,26 @@
 require_relative "test_helper"
 require "./lib/enrollment_repository"
 
-class EnrollmentRepositoryTest
+class EnrollmentRepositoryTest < Minitest::Test
+
+  def test_things
+    skip
+    er = EnrollmentRepository.new
+    er.load_data({
+      :enrollment => {
+        :kindergarten => "./data/Kindergartners in full-day program.csv"
+      }
+    })
+    enrollment = er.find_by_name("ACADEMY 20")
+    # <Enrollment>
+  end
 
   def test_searching_for_a_name_in_enrollment
     e1 = Enrollment.new({:name => "ACADEMY 20"})
     e2 = Enrollment.new({:name => "ACADEMY 30"})
     e3 = Enrollment.new({:name => "ADAMS"})
+    er = EnrollmentRepository.new([e1, e2, e3])
 
-    assert_equal "ADAMS", e3.
+    assert_equal e3, er.find_by_name("ADAMS")
   end
 end
