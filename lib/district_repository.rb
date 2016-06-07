1cf44a821f3575a4f7f79b42dcba084c7afcb11f
Raphael Barbo <rsbarbo@gmail.com>
2016-06-07 14:19:34 -0600

    enrollment up and running

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -3,23 +3,13 @@ require "csv"
 
 class DistrictRepository
 
-
-  def file_opener
-    contents = CSV.open "./data/Kindergartners in full-day program.csv", headers: true, header_converters: :symbol
-    contents.each do |row|
-      location = row[:location]
-      timeframe = row[:timeframe]
-      dataformat = row[:dataformat]
-      data = row[:data]
-    end
-  end
+include CSVUpload
 
   
   def find_by_name
   end
 
 
-  #break up our date into headers that can be called (:district => location)
 
 
 end
--- test/district_repository_test.rb
+++ test/district_repository_test.rb
@@ -1,17 +1,9 @@
 require "./test/test_helper"
-require "./lib/district_repository"
+require_relative "./lib/district_repository"
 
 
 class DistrictRepositoryTest < Minitest::Test
 
-  def test_it_reads_in_the_csv
-    dr = DistrictRepository.new
-    assert_equal 1, dr.file_opener
-  end
 
-  # def test_return_district_name
-  #   dr = DistrictRepository.new
-  #   assert_equal
-  # end
 
 end
