013fbc3425ee07545d2b606b37a806d8379df405
Raphael Barbo <rsbarbo@gmail.com>
2016-06-08 16:37:46 -0600

    done with iteration 0

--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -11,18 +11,13 @@ class DistrictRepository
 
   def load_data(file_tree)
     filepath = file_tree[:enrollment][:kindergarten]
-    name = CSV.foreach(filepath, headers: true, header_converters: :symbol).map do |row|
-      { :name => row[:location], row[:timeframe].to_i => row[:data].to_f}
-    end.group_by do |row|
-      row[:name]
-    end.map do |name, years|
-      merged = years.reduce({}, :merge)
-      merged.delete(:timeframe)
-      { name: name,
-        kindergarten_participation: merged }
-      end.each do |e|
-        @districts << District.new(e)
+    name_lines = CSV.foreach(filepath, headers: true, header_converters: :symbol).map do |row|
+      { :name => row[:location]}
+    end.uniq
+      name_lines.each do |name|
+        @districts << District.new(name)
       end
+      @enrollment_repo.load_data(file_tree)
   end
 
   def find_by_name(name)
