f52d4455f2bf59fc476b456c2162c08f8d03316e
Raphael Barbo <rsbarbo@gmail.com>
2016-06-09 14:48:08 -0600

    almost done with iteration_1 babeeeeegit status

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -1,25 +1,35 @@
 require_relative "district_repository"
 class HeadcountAnalyst
 
-attr_reader :district_repo
+  attr_reader :district_repo
 
-def initialize(dr)
-  @district_repo = dr
-end
+  def initialize(dr)
+    @district_repo = dr
+  end
 
-def kindergarten_participation_rate_variation(first_district, second_district)
-district_one = @district_repo.find_by_name(first_district)
-district_one_years = district_one.enrollment.data[:kindergarten_participation].length
-district_one_total_participation = district_one.enrollment.data[:kindergarten_participation].values.reduce(:+)
-district_one_average = district_one_total_participation/district_one_years
-district_two = @district_repo.find_by_name(second_district[:against])
-district_two_years = district_two.enrollment.data[:kindergarten_participation].length
-district_two_total_participation = district_two.enrollment.data[:kindergarten_participation].values.reduce(:+)
-district_two_average = district_two_total_participation/district_two_years
-rate_variation = district_one_average/ district_two_average
-rate = rate_variation.round(3)
-# needs the trucate method
-end
+  def kindergarten_participation_rate_variation(first_district, second_district)
+    district_one = @district_repo.find_by_name(first_district)
+    district_one_years = district_one.enrollment.data[:kindergarten_participation].length
+    district_one_total_participation = district_one.enrollment.data[:kindergarten_participation].values.reduce(:+)
+    district_one_average = district_one_total_participation/district_one_years
 
+    district_two = @district_repo.find_by_name(second_district[:against])
+    district_two_years = district_two.enrollment.data[:kindergarten_participation].length
+    district_two_total_participation = district_two.enrollment.data[:kindergarten_participation].values.reduce(:+)
+    district_two_average = district_two_total_participation/district_two_years
+    rate_variation = district_one_average/ district_two_average
+    rate = rate_variation.round(3)
+    # needs the trucate method
+  end
 
+  def kindergarten_participation_rate_variation_trend(first_district, second_district)
+    district_one = @district_repo.find_by_name(first_district)
+    district_one_years = district_one.enrollment.data[:kindergarten_participation]
+
+    district_two = @district_repo.find_by_name(second_district[:against])
+    district_two_years = district_two.enrollment.data[:kindergarten_participation]
+
+    varitation_trend = district_one_years.merge!(district_two_years) { |key , onekey, twokey| ((onekey / twokey) * 1000).floor / 1000.to_f}
+    trend = varitation_trend.sort.to_h
+  end
 end
