546ca28fd4e68575f278adc1620493bafbcf43ad
Raphael Barbo <rsbarbo@gmail.com>
2016-06-12 19:13:33 -0600

    great team work

--- test/enrollment_repository_test.rb
+++ test/enrollment_repository_test.rb
@@ -26,7 +26,7 @@ class EnrollmentRepositoryTest < Minitest::Test
     assert_equal e3, er.find_by_name("ADAMS")
   end
 
-  def test_it_takes_multiple_paths
+  def test_graduation_rate_by_year
     er = EnrollmentRepository.new
     er.load_data({
       :enrollment => {
@@ -44,4 +44,19 @@ class EnrollmentRepositoryTest < Minitest::Test
 
     assert_equal result, enrollment.graduation_rate_by_year
   end
+
+  def test_graduation_rate_in_year
+    er = EnrollmentRepository.new
+    er.load_data({
+      :enrollment => {
+        :kindergarten => "./data/Kindergartners in full-day program.csv",
+        :high_school_graduation => "./data/High school graduation rates.csv"
+      }
+    })
+    enrollment = er.find_by_name("ACADEMY 20")
+
+
+    assert_equal 0.895, enrollment.graduation_rate_in_year(2010)
+  end
+
 end
--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -43,4 +43,33 @@ class HeadcountAnalystTest < Minitest::Test
 
     assert_equal result, ha.kindergarten_participation_rate_variation_trend('ACADEMY 20', :against => 'COLORADO')
   end
+
+  def test_something_something
+    dr = DistrictRepository.new
+    dr.load_data({
+      :enrollment => {
+        :kindergarten => "./data/Kindergartners in full-day program.csv",
+        :high_school_graduation => "./data/High school graduation rates.csv"
+      }
+    })
+    ha = HeadcountAnalyst.new(dr)
+
+    assert_equal 0.452,ha.kindergarten_participation_against_high_school_graduation('ACADEMY 20')
+    #1.234
+  end
+
+  def test_kindergarten_participation_correlates_with_high_school_graduation
+    dr = DistrictRepository.new
+    dr.load_data({
+      :enrollment => {
+        :kindergarten => "./data/Kindergartners in full-day program.csv",
+        :high_school_graduation => "./data/High school graduation rates.csv"
+      }
+    })
+    ha = HeadcountAnalyst.new(dr)
+
+    assert_equal true, ha.kindergarten_participation_correlates_with_high_school_graduation(for: 'ACADEMY 20')
+  end
+
+
 end
