a09b3ad2366f61a4ec1d9f26868344f950ee2947
Lane Winham <mlwinham12@gmail.com>
2016-06-16 08:57:47 -0600

    final touches

--- lib/economic_profile_repository.rb
+++ lib/economic_profile_repository.rb
@@ -1,89 +1,73 @@
+
 require_relative "economic_profile"
 require "csv"
 
 class EconomicProfileRepository
-
-  attr_reader :economic_profile
+  attr_reader :economic_profile,
+              :data_sets
 
   def initialize(economic_profile = {})
     @economic_profile = economic_profile
+    @data_sets =  {
+      :median_household_income => :median_household_income,
+      :children_in_poverty => :children_in_poverty,
+      :free_or_reduced_price_lunch => :free_or_reduced_price_lunch,
+      :title_i => :title_i
+      }
   end
 
-  def load_data(data)
-    stored_data = {}
-    data[:economic_profile].each do |title, file_path|
-      CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
-        name = row[:location].upcase
-        dataformat = row[:dataformat]
-        data = row[:data].to_f
-        timeframe = row[:timeframe]
-
-        if title == :free_or_reduced_price_lunch
-          if dataformat == "Percent"
-            percentage = data.to_f
-          elsif  dataformat == "Number"
-            total = data.to_i
-          end
-        end
-
-        if title == :median_household_income
-          if dataformat == "Currency"
-            total = data.to_i
-            years = timeframe.split("-").map { |num| num.to_i }
-            income = data.to_i
-          else
-            years = timeframe.to_i
-            percentage = data.to_f
-          end
-        end
-
-        if title == :free_or_reduced_price_lunch
-          lunch_parsed_data(stored_data, name, title, years, percentage, total, dataformat)
-        elsif title == :median_household_income
-          additional_parser(stored_data, name, title, years, income)
-        else title == :children_in_poverty || :title_i
-          additional_parser(stored_data, name, title, years, percentage)
-        end
-      end
-    end
-  end
-
-  def additional_parser(stored_data, name, title, years, income)
-    if stored_data.has_key?(name)
-      if stored_data[name].has_key?(title)
-        if stored_data[name][title].has_key?(years)
-          stored_data[name][title][years] = percentage
+  def load_data(file_path)
+    file_path.values[0].values.each_with_index do |key, index|
+      CSV.foreach(key, headers: true, header_converters: :symbol) do |row|
+        title = data_sets[file_path.values[0].keys[index]]
+        data = path_to_parsing(title, row)
+        name = data[:name]
+        check_objects = find_by_name(name)
+        if check_objects == nil
+          @economic_profile[name.upcase] = EconomicProfile.new(data)
         else
-          stored_data[name][year] = {title => percentage}
+          add_data(check_objects, title, data)
         end
-      else
-        stored_data[name] = {year => {title => percentage}}
       end
     end
   end
 
-  def lunch_parsed_data(stored_data, name, title, years, percentage, total, dataformat)
-    if stored_data.has_key?(name)
-      if stored_data[name].has_key?(title)
-        if stored_data[name][title].has_key?(years)
-          stored_data[name][title][years] = percentage
-        else
-          stored_data[name][years] = {title => percentage}
-        end
-      else
-        stored_data[name] = {years => {title => percentage}}
-      end
+  def path_to_parsing(title, row)
+    if title == :median_household_income || title == :title_i
+      name, year, value = parse_row(title, row)
+      { :name => name, title => {year => value}}
+    elsif title == :children_in_poverty
+      name, year, value = parse_row(title, row)
+      { :name => name, title => {year => value}}
+    elsif title == :free_or_reduced_price_lunch
+      name, year, type, value = parse_row_poverty_and_lunch(title, row)
+      { :name => name, title => {year => {type.to_sym => value}}}
     end
-    create_economic_profile(stored_data)
   end
 
-  def create_economic_profile(data)
-    data.each do |name, district_data|
-      economic_profile.merge!(name => EconomicProfile.new(district_data))
-    end
+  def parse_row(title, row)
+    name = row[:location].upcase
+    year = row[:timeframe].to_i
+     if title == :median_household_income
+       year = row[:timeframe].split("-").map { |num| num.to_i }
+     end
+    value = row[:data].to_f
+    [name, year, value]
+  end
+
+  def parse_row_poverty_and_lunch(title, row)
+    name = row[:location].upcase
+    year = row[:timeframe].to_i
+    type = row[:dataformat]
+    value = row[:data].to_f
+    [name, year, type, value]
+  end
+
+  def add_data(check_objects, title, data)
+
   end
 
   def find_by_name(name)
-    economic_profile[name.upcase]
+    @economic_profile[name.upcase]
   end
 end
